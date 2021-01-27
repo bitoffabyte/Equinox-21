9b5a80418f6a726ff20eafe2e98f66f9825fc643
R Narayan <nr.rnarayan@gmail.com>
2021-01-27 19:51:42 +0530

    Added bg-colors :sparkles:

--- src/App.js
+++ src/App.js
@@ -4,11 +4,48 @@ function App() {
 	return (
 		<div className='App'>
 			<Earth />
-			<div style={{ height: '100vh' }}></div>
-			<div style={{ height: '100vh' }}></div>
-			<div style={{ height: '100vh' }}></div>
-			<div style={{ height: '100vh' }}></div>
-			<div style={{ height: '100vh' }}></div>
+			<div
+				style={{
+					height: '100vh',
+					width: '100vw',
+					backgroundImage: `linear-gradient(180deg, #061891, #4db6e0)`,
+				}}
+			></div>
+			<div
+				style={{
+					height: '100vh',
+					width: '100vw',
+					backgroundImage: `linear-gradient(180deg, #4db6e0, #f6d686)`,
+				}}
+			></div>
+			<div
+				style={{
+					height: '100vh',
+					width: '100vw',
+					backgroundImage: `linear-gradient(180deg, #f6d686, #fc965a)`,
+				}}
+			></div>
+			<div
+				style={{
+					height: '100vh',
+					width: '100vw',
+					backgroundImage: `linear-gradient(180deg, #fc965a, #b75c99)`,
+				}}
+			></div>
+			<div
+				style={{
+					height: '100vh',
+					width: '100vw',
+					backgroundImage: `linear-gradient(180deg, #b75c99, #72439a)`,
+				}}
+			></div>
+			<div
+				style={{
+					height: '100vh',
+					width: '100vw',
+					backgroundImage: `linear-gradient(180deg, #72439a, #171e92)`,
+				}}
+			></div>
 		</div>
 	);
 }
