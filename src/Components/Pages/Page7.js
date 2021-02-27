09ce5c0b3393e27c644a866f066d46659f0966e1
R Narayan <nr.rnarayan@gmail.com>
2021-02-27 22:28:17 +0530

    added links :sparkles:

--- src/Components/Pages/Page7.js
+++ src/Components/Pages/Page7.js
@@ -16,7 +16,7 @@ import voi from '../../Assets/Sponsors/voice.svg';
 import jb from '../../Assets/Sponsors/jetbrains.svg';
 import gfg from '../../Assets/Sponsors/GFG LOGO-1.png';
 import intb from '../../Assets/Sponsors/logo1.png';
-import codex from '../../Assets/Sponsors/Codex_LOGO.png';
+import codex from '../../Assets/Sponsors/codex.svg';
 
 import test from '../../Assets/Sponsors/text-logo.svg';
 import wic from '../../Assets/Sponsors/wic_long_white.png';
@@ -30,6 +30,28 @@ import arrow from '../../Assets/Arrow.svg';
 import { memo, useState } from 'react';
 const Page7 = ({ fadeIn }) => {
 	const [page, updatePage] = useState(1);
+	const links = [
+		'https://www.amul.com/products/amul-amulya-info.php',
+		'https://www.codingninjas.com/',
+		'https://www.digitalocean.com/',
+		'https://www.framer.com/',
+		'https://www.wolframalpha.com/',
+		'https://www.bugsee.com/',
+		'https://www.jetbrains.com/',
+		'https://www.voiceflow.com/',
+		'https://www.taskade.com/',
+		'https://www.sashido.io/en/',
+		'https://balsamiq.com/',
+		'https://www.geeksforgeeks.org/',
+		'https://interviewbuddy.in/',
+		'https://thecodex.me/',
+		'https://testmail.app/',
+		'wic.in', // not sure
+		'https://www.girlscript.tech/home',
+		'https://www.sketch.com/',
+		'yay.in', // not sure
+		'https://indiablockchainalliance.org/',
+	];
 	return (
 		<div className='page7bg'>
 			<div className={`Page7Content ${fadeIn ? 'fadeIn' : ''} `}>
@@ -37,31 +59,128 @@ const Page7 = ({ fadeIn }) => {
 
 				<div className='sponsors '>
 					<div className={`page ${page == 1 ? 'show' : ''}`}>
-						<div className='line1'>
-							<img src={am} className='imgs' />
-							<img src={cn} className='imgs' />
-							<img src={doo} className='imgs' />
-							<img src={fr} className='imgs' />
-							<img src={wo} className='imgs' />
-							<img src={bg} className='imgs' />
-							<img src={jb} className='imgs jb' />
-							<img src={voi} className='imgs' />
-							<img src={tas} className='imgs' />
-							<img src={sa} className='imgs' />
-							<img src={bal} className='imgs' />
-							<img src={gfg} className='imgs' />
-							<img src={intb} className='imgs' />
-							<img src={codex} className='imgs' />
+						<div className='line1' style={{ zIndex: 1000 }}>
+							<a href={links[0]} className='imgs' target='_blank'>
+								<img src={am} className='imgs' />
+							</a>
+							<a href={links[1]} className='imgs' target='_blank'>
+								<img src={cn} className='imgs' />
+							</a>
+
+							<a href={links[2]} className='imgs' target='_blank'>
+								<img src={doo} className='imgs' />
+							</a>
+
+							<a href={links[3]} className='imgs' target='_blank'>
+								<img src={fr} className='imgs' />
+							</a>
+
+							<a href={links[4]} className='imgs' target='_blank'>
+								<img src={wo} className='imgs' />
+							</a>
+
+							<a href={links[5]} className='imgs' target='_blank'>
+								<img src={bg} className='imgs' />
+							</a>
+
+							<a href={links[6]} className='imgs' target='_blank'>
+								<img src={jb} className='imgs jb' />
+							</a>
+
+							<a href={links[7]} className='imgs' target='_blank'>
+								<img src={voi} className='imgs' />
+							</a>
+
+							<a href={links[8]} className='imgs' target='_blank'>
+								<img src={tas} className='imgs' />
+							</a>
+
+							<a href={links[9]} className='imgs' target='_blank'>
+								<img src={sa} className='imgs' />
+							</a>
+
+							<a
+								href={links[10]}
+								className='imgs'
+								target='_blank'
+							>
+								<img src={bal} className='imgs' />
+							</a>
+
+							<a
+								href={links[11]}
+								className='imgs'
+								target='_blank'
+							>
+								<img src={gfg} className='imgs' />
+							</a>
+
+							<a
+								href={links[12]}
+								className='imgs'
+								target='_blank'
+							>
+								<img src={intb} className='imgs' />
+							</a>
+
+							<a
+								href={links[13]}
+								className='imgs'
+								target='_blank'
+							>
+								<img src={codex} className='imgs' />
+							</a>
 						</div>
 					</div>
-					<div className={`page ${page == 2 ? 'show' : ''}`}>
+					<div className={`page${page == 2 ? ' show' : ''}`}>
 						<div className='line1'>
-							<img src={test} className='imgs' />
-							<img src={wic} className='imgs jb' />
-							<img src={gs} className='imgs' />
-							<img src={sk} className='imgs' />
-							<img src={ya} className='imgs' />
-							<img src={iba} className='imgs' />
+							<a
+								href={links[14]}
+								className='imgs'
+								target='_blank'
+							>
+								<img src={test} className='imgs' />
+							</a>
+
+							<a
+								href={links[15]}
+								className='imgs'
+								target='_blank'
+							>
+								<img src={wic} className='imgs jb' />
+							</a>
+
+							<a
+								href={links[16]}
+								className='imgs'
+								target='_blank'
+							>
+								<img src={gs} className='imgs' />
+							</a>
+
+							<a
+								href={links[17]}
+								className='imgs'
+								target='_blank'
+							>
+								<img src={sk} className='imgs' />
+							</a>
+
+							<a
+								href={links[18]}
+								className='imgs'
+								target='_blank'
+							>
+								<img src={ya} className='imgs' />
+							</a>
+
+							<a
+								href={links[19]}
+								className='imgs'
+								target='_blank'
+							>
+								<img src={iba} className='imgs' />
+							</a>
 						</div>
 					</div>
 				</div>
