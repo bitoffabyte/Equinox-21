f450936a03d747db89c376e4cd21be48f718be0f
R Narayan <nr.rnarayan@gmail.com>
2021-04-08 16:57:30 +0530

    Loading :construction:

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, Component } from 'react';
 import Load from './Components/Styles/Load';
 import Main from './Main';
 
-const App = () => {
-	const [load, setLoad] = useState(true);
-	useEffect(() => {
-		setLoad(false);
-	}, []);
-	return (
-		<div>
-			{load ? <Load /> : null}
-			<Main />
-		</div>
-	);
-};
+class App extends Component {
+	state = {
+		loading: true,
+	};
+	componentDidMount() {
+		// alert('asd');
+		this.setState({ loading: false });
+	}
+	render() {
+		return (
+			<div>
+				{this.state.load ? <Load /> : null}
+				<Main />
+			</div>
+		);
+	}
+}
 
 export default App;
