5df06b621aee7b74040862fdc1df71a43d91c1d7
R Narayan <nr.rnarayan@gmail.com>
2021-04-09 15:41:57 +0530

    Added new faqs :sparkles:

--- src/Components/Pages/Page5.js
+++ src/Components/Pages/Page5.js
@@ -4,6 +4,8 @@ import { useState, memo } from 'react';
 import arrow from '../../Assets/Arrow.svg';
 import Accordion from '../Accordion';
 import { FAQ as data } from './faq-data';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import { Carousel } from 'react-responsive-carousel';
 const Page5 = ({ fadeIn }) => {
 	const { width, height } = useDimension();
 	const [show1, updateShow1] = useState(false);
@@ -12,7 +14,15 @@ const Page5 = ({ fadeIn }) => {
 	const [show4, updateShow4] = useState(false);
 	const [show5, updateShow5] = useState(false);
 	const [show6, updateShow6] = useState(false);
-	const [page, updatePage] = useState(1);
+	const [show7, updateShow7] = useState(false);
+	const [show8, updateShow8] = useState(false);
+	const [show9, updateShow9] = useState(false);
+	const [show10, updateShow10] = useState(false);
+	const [show11, updateShow11] = useState(false);
+	const [show12, updateShow12] = useState(false);
+	const [show13, updateShow13] = useState(false);
+	const [page, updatePage] = useState(0);
+
 	const updateHandler = (func, s) => {
 		updateShow1(false);
 		updateShow2(false);
@@ -20,76 +30,287 @@ const Page5 = ({ fadeIn }) => {
 		updateShow4(false);
 		updateShow5(false);
 		updateShow6(false);
+		updateShow7(false);
+		updateShow8(false);
+		updateShow9(false);
+		updateShow10(false);
+		updateShow11(false);
+		updateShow12(false);
+		updateShow13(false);
 		func(s);
 	};
+
 	return (
 		<div className='page5bg'>
 			<div className={`Page5Content ${fadeIn ? 'fadeIn' : ''} `}>
 				<h1>FAQs</h1>
-				<div className='faqs'>
-					<div className={`leftfaq ${page == 1 ? 'pageReveal' : ''}`}>
-						<Accordion
-							title={data[0].q}
-							body={data[0].a}
-							state={show1}
-							updateState={updateShow1}
-							updateHandler={updateHandler}
-							s={true}
-						/>
-						<Accordion
-							title={data[1].q}
-							body={data[1].a}
-							state={show2}
-							updateState={updateShow2}
-							updateHandler={updateHandler}
-						/>
-						<Accordion
-							title={data[2].q}
-							body={data[2].a}
-							state={show3}
-							updateState={updateShow3}
-							updateHandler={updateHandler}
-						/>
-					</div>
-					<div
-						className={`rightfaq ${page == 2 ? 'pageReveal' : ''}`}>
-						<Accordion
-							title={data[3].q}
-							body={data[3].a}
-							state={show4}
-							updateState={updateShow4}
-							updateHandler={updateHandler}
-						/>
-						<Accordion
-							title={data[4].q}
-							body={data[4].a}
-							state={show5}
-							updateState={updateShow5}
-							updateHandler={updateHandler}
-						/>
-						<Accordion
-							title={data[5].q}
-							body={data[5].a}
-							state={show6}
-							updateState={updateShow6}
-							updateHandler={updateHandler}
-						/>
-					</div>
-					{width < 720 ? (
-						<div className='btnBt'>
-							<img
-								src={arrow}
-								style={{ transform: 'rotate(180deg)' }}
-								className={`btn ${page == 1 ? 'dis' : ''}`}
-								onClick={() => updatePage(1)}
+
+				{width > 720 ? (
+					<Carousel
+						autoPlay={false}
+						emulateTouch={true}
+						showIndicators={false}
+						showArrows={false}
+						showStatus={false}
+						transitionTime={200}
+						selectedItem={page}
+						emulateTouch={false}
+						swipeable={false}
+						className='carou'>
+						<div>
+							<Accordion
+								title={data[0].q}
+								body={data[0].a}
+								state={show1}
+								updateState={updateShow1}
+								updateHandler={updateHandler}
+								s={true}
 							/>
-							<img
-								src={arrow}
-								className={`btn ${page == 2 ? 'dis' : ''}`}
-								onClick={() => updatePage(2)}
+							<Accordion
+								title={data[1].q}
+								body={data[1].a}
+								state={show2}
+								updateState={updateShow2}
+								updateHandler={updateHandler}
+							/>
+							<Accordion
+								title={data[2].q}
+								body={data[2].a}
+								state={show3}
+								updateState={updateShow3}
+								updateHandler={updateHandler}
+							/>
+
+							<Accordion
+								title={data[3].q}
+								body={data[3].a}
+								state={show4}
+								updateState={updateShow4}
+								updateHandler={updateHandler}
+							/>
+							<Accordion
+								title={data[4].q}
+								body={data[4].a}
+								state={show5}
+								updateState={updateShow5}
+								updateHandler={updateHandler}
 							/>
 						</div>
-					) : null}
+						<div>
+							<Accordion
+								title={data[5].q}
+								body={data[5].a}
+								state={show6}
+								updateState={updateShow6}
+								updateHandler={updateHandler}
+							/>
+							<Accordion
+								title={data[6].q}
+								body={data[6].a}
+								state={show7}
+								updateState={updateShow7}
+								updateHandler={updateHandler}
+							/>
+							<Accordion
+								title={data[7].q}
+								body={data[7].a}
+								state={show8}
+								updateState={updateShow8}
+								updateHandler={updateHandler}
+							/>
+							<Accordion
+								title={data[8].q}
+								body={data[8].a}
+								state={show9}
+								updateState={updateShow9}
+								updateHandler={updateHandler}
+							/>
+							<Accordion
+								title={data[9].q}
+								body={data[9].a}
+								state={show10}
+								updateState={updateShow10}
+								updateHandler={updateHandler}
+							/>
+						</div>
+
+						<div>
+							<Accordion
+								title={data[10].q}
+								body={data[10].a}
+								state={show11}
+								updateState={updateShow11}
+								updateHandler={updateHandler}
+							/>
+							<Accordion
+								title={data[11].q}
+								body={data[11].a}
+								state={show12}
+								updateState={updateShow12}
+								updateHandler={updateHandler}
+							/>
+							<Accordion
+								title={data[12].q}
+								body={data[12].a}
+								state={show13}
+								updateState={updateShow13}
+								updateHandler={updateHandler}
+							/>
+						</div>
+					</Carousel>
+				) : (
+					<Carousel
+						autoPlay={false}
+						emulateTouch={true}
+						showIndicators={false}
+						showArrows={false}
+						showStatus={false}
+						transitionTime={200}
+						selectedItem={page}
+						emulateTouch={false}
+						swipeable={false}
+						className='carou'>
+						<div>
+							<Accordion
+								title={data[0].q}
+								body={data[0].a}
+								state={show1}
+								updateState={updateShow1}
+								updateHandler={updateHandler}
+								s={true}
+							/>
+							<Accordion
+								title={data[1].q}
+								body={data[1].a}
+								state={show2}
+								updateState={updateShow2}
+								updateHandler={updateHandler}
+							/>
+							<Accordion
+								title={data[2].q}
+								body={data[2].a}
+								state={show3}
+								updateState={updateShow3}
+								updateHandler={updateHandler}
+							/>
+						</div>
+						<div>
+							<Accordion
+								title={data[3].q}
+								body={data[3].a}
+								state={show4}
+								updateState={updateShow4}
+								updateHandler={updateHandler}
+							/>
+							<Accordion
+								title={data[4].q}
+								body={data[4].a}
+								state={show5}
+								updateState={updateShow5}
+								updateHandler={updateHandler}
+							/>
+							<Accordion
+								title={data[5].q}
+								body={data[5].a}
+								state={show6}
+								updateState={updateShow6}
+								updateHandler={updateHandler}
+							/>
+						</div>
+
+						<div>
+							<Accordion
+								title={data[6].q}
+								body={data[6].a}
+								state={show7}
+								updateState={updateShow7}
+								updateHandler={updateHandler}
+							/>
+							<Accordion
+								title={data[7].q}
+								body={data[7].a}
+								state={show8}
+								updateState={updateShow8}
+								updateHandler={updateHandler}
+							/>
+							<Accordion
+								title={data[8].q}
+								body={data[8].a}
+								state={show9}
+								updateState={updateShow9}
+								updateHandler={updateHandler}
+							/>
+						</div>
+						<div>
+							<Accordion
+								title={data[9].q}
+								body={data[9].a}
+								state={show10}
+								updateState={updateShow10}
+								updateHandler={updateHandler}
+							/>
+							<Accordion
+								title={data[10].q}
+								body={data[10].a}
+								state={show11}
+								updateState={updateShow11}
+								updateHandler={updateHandler}
+							/>
+							<Accordion
+								title={data[11].q}
+								body={data[11].a}
+								state={show12}
+								updateState={updateShow12}
+								updateHandler={updateHandler}
+							/>
+						</div>
+						<div>
+							<Accordion
+								title={data[12].q}
+								body={data[12].a}
+								state={show13}
+								updateState={updateShow13}
+								updateHandler={updateHandler}
+							/>
+						</div>
+					</Carousel>
+				)}
+				<div className='asssd'>
+					<img
+						src={arrow}
+						style={{ transform: 'rotate(180deg)' }}
+						className='btn'
+						onClick={() =>
+							updatePage((prev) => {
+								if (prev == 0) {
+									return prev;
+								} else {
+									return prev - 1;
+								}
+							})
+						}
+					/>
+					<img
+						src={arrow}
+						className='btn'
+						onClick={() =>
+							updatePage((prev) => {
+								if (width > 720)
+									if (prev == 2) {
+										return 2;
+									} else {
+										return prev + 1;
+									}
+								else {
+									if (prev == 4) {
+										return 4;
+									} else {
+										return prev + 1;
+									}
+								}
+							})
+						}
+					/>
 				</div>
 			</div>
 		</div>
--- src/Components/Pages/faq-data.js
+++ src/Components/Pages/faq-data.js
@@ -3,29 +3,64 @@ export const FAQ = [
 		q: 'What are the rules?',
 		a: `We want our Hack to be fun and fair to everyone. Your project should be started from scratch and worked on only during the 36 hours, and you may only incorporate outside resources (code, graphics, sound, etc) that are freely licensed and accessible to all participants equally. Of course, maintain a positive, respectful attitude toward your fellow hackers throughout the event. We ask that all attendees follow the`,
 	},
+
 	{
-		q: 'What can I build?',
-		a: `You can build whatever your heart desires,
-								whether it’s a web app, mobile app, hardware
-								hack, or an open source tool. Our main focus is
-								to bring you a learning experience where you can
-								have fun, ask questions, and experiment with
-								technology.`,
+		q: 'What are the perks of participating in Equinox?',
+		a: `A plethora of knowledge, keen problems solving skills and a rich technical skillset are in the cards! Additionally, Equinox will also serve as a platform to mingle with others in the tech community. Cash prizes, schawgs and other perks are in store for the winners!`,
+	},
+
+	{
+		q: 'Can I start working on the hack before the event?',
+		a: `You can research thoroughly on your ideas before the start of the event, but we insist you start working on the projects only after the hack begins to be fair to each participant and to build on your time management too!`,
+	},
+
+	{
+		q: 'How big can my team be? ',
+		a: `You can register solo but as they say “the more the merrier”, so we advise you to show up with 2-5 stars in your cluster. If you do not have a team, you can interact with the other participants over our Discord Server and form your ideal cluster of stars!`,
+	},
+
+	{
+		q: 'Do I have to pay to register?',
+		a: `Not at all! Thanks to the generosity of our sponsors, we are able to conduct events like Equinox for free.`,
+	},
+
+	{
+		q: 'Who all can participate?',
+		a: `Equinox’21 is open to all the students who are currently enrolled in a University, or have graduated within the last 12 months.`,
+	},
+
+	{
+		q: 'What if I don’t know how to code?',
+		a: `In this never ending world where technologies and innovation are the future, all you need is the  will to learn, the curiosity to explore and determination to bring your idea to life. Equinox is the right platform to kick start your coding journey.`,
+	},
+
+	{
+		q: 'What if this is my first hack?',
+		a: `Whether you’re new to hacks or a seasoned pro, there’s something for everyone! In this case, Equinox will prove to be the Big Bang to your hacking journey. You can hone your skills with creativity and zeal, and take back with you substantial knowledge and experience.  `,
 	},
 	{
-		q: 'Are hardware hacks allowed?',
-		a: `Yes, of course!`,
+		q: 'What can I build in the hack? ',
+		a: `If you can think it, you can build it! Be it websites, android apps, hardware or electronics, these are just the tip of the iceberg. Express your creativity, ingenuity and determination to explore anything you want. `,
 	},
+
 	{
-		q: 'How much does it cost?',
-		a: `Thanks to the generosity of our sponsors, we’re able to put up events like these for free.`,
+		q: 'Who will own my ideas and projects?',
+		a: `We know that you put your heart and soul into your ideas and their implementations, so you will be the sole owners of your projects.`,
 	},
+
 	{
-		q: 'How many people in a team?',
-		a: `Two to Five members per team.`,
+		q: 'When do registrations close?',
+		a: `You have till 25th March, 2021, 23:59 IST to register yourself for the blast off to the best 36 hours of hacking you will ever have. `,
 	},
+
 	{
-		q: 'I still have a question...',
-		a: `We’d be happy to help! Send the inquiry over to robovitics@vit.ac.in and we’ll get back to you as soon as possible or call +91 98673 48865.`,
+		q: 'What is the venue of the hackathon?',
+		a: `Anywhere on the face of the earth that you find comfortable, with ample charging ports, while making sure you follow social distancing norms.`,
+	},
+
+	{
+		q: '404: Question not found, maybe a black hole ate it?',
+		a: `Don’t worry you stray comet, we won’t let you be lost. We’ve got your back! Send in your query
+        over to equinox.robovitics@gmail.com or contact +91 98980 56388 and we will be more than happy to help!`,
 	},
 ];
